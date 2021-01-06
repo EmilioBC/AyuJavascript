c989d9bca8e5192315d7277fbe110501424a9d7c
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-01-06 05:36:27 +0000

    jsup

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-alert("Hello World");
+// var post = prompt("Compose your message of upto 140 characters.");
+// var postSlice = post.slice(0,140);
+// var postCount = postSlice.length;
+// var remaining = 140 - postSlice.length;
+// alert(postSlice + " \nYou have written " + postCount + " characters and you have " + remaining + " left");
+
+// var name = prompt("What is your name?");
+// name = name.toLowerCase();
+// var nameSlice = name.slice(0,1);
+// var firstUpper = nameSlice.toUpperCase();
+// var restLower = name.slice(1,name.length);
+// alert("Hello " + firstUpper + restLower);
+
+// Dog Age
+// var dogAge = prompt("How old is the dog?");
+// var humanAge = (dogAge - 2) * 4 + 21;
+// alert("You dog is " + dogAge + " years old and it is " + humanAge + " years old in humans age." );
+
+
+function getMilk(bottles) {   
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("buy " + bottles + " bottles of milk");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+} 
+
+getMilk(12);
