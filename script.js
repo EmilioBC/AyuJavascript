4716717f72632e22dd9ed20ba2c64c42ad9d20b6
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-01-07 22:41:50 +0000

    bmi

--- script.js
+++ script.js
@@ -195,10 +195,41 @@ bmi should equal 20 when it's rounded to the nearest whole number.
 
 /////////// LOVE CALCULATOR ////////////////////////
 
-  var yourName = prompt("What is your Name? ");
-  var crushName = prompt("What is your crush Name? ");
+//   var yourName = prompt("What is your Name? ");
+//   var crushName = prompt("What is your crush Name? ");
 
-  var randomNum = Math.random();
-  randomNum = Math.floor(randomNum * 100) + 1;
+//   var loveScore = Math.random();
+//   loveScore = Math.floor(loveScore * 100) + 1;
   
-alert("You are " + randomNum + "% compatible");
+// alert("You are " + loveScore + "% compatible");
+
+
+////////// BMI CALCULATOR /////////////////
+
+////////////// BMI CALCULATOR ///////////////////////////
+
+//Create your function below this line.
+//The first parameter should be the weight and the second should be the height.
+
+function bmiCalculator(weight, height) {
+   var bmi = Math.floor(weight / (height * height));
+   var interpretation; 
+
+    if (bmi < 18.5) {
+      interpretation = ("Your BMI is " + bmi + ", so you are underweight."  );
+    } 
+    if (bmi >= 18.5 && bmi <= 24.9) {
+      interpretation = ("Your BMI is " + bmi + ", so you have a normal weight.");
+    }
+    if (bmi > 24.9) {
+      interpretation = ("Your BMI is " + bmi + ", so you are overweight.");
+    }
+    return interpretation;
+}
+
+ alert(bmiCalculator(60, 2));
+
+
+/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+*/
+// var bmi = bmiCalculator(65, 1.8); 
