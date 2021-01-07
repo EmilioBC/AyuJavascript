f702031d25d303b60bb1ed394b03d076a459e9f2
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-01-07 01:58:04 +0000

    random

--- script.js
+++ script.js
@@ -177,13 +177,13 @@
 //Create your function below this line.
 //The first parameter should be the weight and the second should be the height.
 
-function bmiCalculator(weight, height) {
-  var calcBMI = Math.floor(weight / (height * height));
-  return calcBMI;
-}
+// function bmiCalculator(weight, height) {
+//   var calcBMI = Math.floor(weight / (height * height));
+//   return calcBMI;
+// }
 
-var bmi = bmiCalculator(65, 1.8);
-console.log(bmi);
+// var bmi = bmiCalculator(65, 1.8);
+// console.log("Your BMI is: " + bmi);
 
 /* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
 
@@ -192,3 +192,13 @@ var bmi = bmiCalculator(65, 1.8);
 bmi should equal 20 when it's rounded to the nearest whole number.
 
 */
+
+/////////// LOVE CALCULATOR ////////////////////////
+
+  var yourName = prompt("What is your Name? ");
+  var crushName = prompt("What is your crush Name? ");
+
+  var randomNum = Math.random();
+  randomNum = Math.floor(randomNum * 100) + 1;
+  
+alert("You are " + randomNum + "% compatible");
