a08c15f670fdb6ffbdb3b22f394cd0b74711b005
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-01-09 07:11:09 +0000

    fizzBuzz

--- script.js
+++ script.js
@@ -259,3 +259,32 @@ bmi should equal 20 when it's rounded to the nearest whole number.
 // }
 
 //////////////////////////////////////////////////////////
+
+
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+
+  if (count % 3 === 0 && count % 5 === 0) {
+    output.push("FizzBuzz");
+  } else if (count % 3 === 0) {
+    output.push("Fizz");
+  } else if (count % 5 === 0) {
+    output.push("Buzz"); 
+  } else {
+    output.push(count);
+  }
+
+  count++;
+
+  console.log(output);
+
+  // if (count % 5 === 0) {
+  //   count = ("Buzz");
+  // }
+  // if (count % 3 === 0 && count % 5 === 0) {
+  //   count = ("FizzBuzz");
+  // }
+  // return;
+}
